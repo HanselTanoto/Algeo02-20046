14d6e012c9cc23f5eaf2a486d33e4fb67ff94172
HanselTanoto <hansel.tanoto05@gmail.com>
2021-11-09 20:37:58 +0700

    Update svd

--- svd.py
+++ svd.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 from PIL import Image
 import timeit
@@ -6,104 +7,83 @@ import cv2
 # Timer Start
 start = timeit.default_timer()
 
-# convert image to array
-image = Image.open(r"Test Image\Chrome_Logo.jpg")
-Arr = np.array(image)
+import math
+import numpy as np
+from PIL import Image
+import timeit
+import cv2
+
+def svd(A, k):
+    A_T = np.transpose(A)
+    A_A_T = np.dot(A, A_T)
+    A_T_A = np.dot(A_T, A)
+    m, n = A.shape
+    new_m = int(k/100*m)
+    new_n = int(k/100*n)
+    # Matriks U
+    e_val1, e_vec1 = np.linalg.eigh(A_A_T)
+    e_val1 = np.flip(e_val1, axis=0)
+    e_vec1 = np.flip(e_vec1, axis=0)
+    U = np.zeros((m,new_m))
+    for i in range(m):
+        for j in range(new_m):
+            U[i][j] = e_vec1[i][j]
+    # Matriks V dan Sigma
+    e_val2, e_vec2 = np.linalg.eigh(A_T_A)
+    e_val2 = np.flip(e_val2, axis=0)
+    e_vec2 = np.flip(e_vec2, axis=0)
+    """
+    V = np.zeros((n,n))
+    for i in range(n):
+        for j in range(n):
+            V[i][j] = e_vec2[i][j]
+    V_T = np.transpose(V)
+    """
+    k = min(new_m,new_n)
+    S = np.zeros((new_m,new_n))
+    for i in range(k):
+        S[i][i] = math.sqrt(e_val2[i])
+    e_vec1_i = np.linalg.inv(e_vec1)
+    U_i = np.zeros((new_m,m))
+    for i in range(new_m):
+        for j in range(m):
+            U_i[i][j] = e_vec1_i[i][j]
+    S_i = np.zeros((new_n,new_m))
+    for i in range(k):
+        S_i[i][i] = 1/math.sqrt(e_val2[i])
+    V_T = np.dot(U_i,A)
+    V_T = np.dot(S_i,V_T)
+    print(U.shape)
+    print(S.shape)
+    print(V_T.shape)
+    return U, S, V_T
+
+
+image = Image.open(r"Test Image\Coffee.jpg")
+Arr = np.asarray(image).astype(float)
 print(Arr)
-m,n,c = Arr.shape
-print(m,n,c)    # dimensi array : (panjang,lebar,dimensi_warna) 
-# split array to r g b array
+
 Arr_b, Arr_g, Arr_r = cv2.split(Arr)
-print(Arr_b)
-print(Arr_g)
-print(Arr_r)
-# transpose dari r, g, b array
-Arr_b_T = np.transpose(Arr_b)
-Arr_g_T = np.transpose(Arr_g)
-Arr_r_T = np.transpose(Arr_r)
-# hitung transpose(A).A untuk cari matriks V dan Sigma
-Arr_T_Arr_b = np.dot(Arr_b_T,Arr_b)
-Arr_T_Arr_g = np.dot(Arr_g_T,Arr_g)
-Arr_T_Arr_r = np.dot(Arr_r_T,Arr_r)
-# hitung A.transpose(A) untuk cari matriks U
-Arr_Arr_T_b = np.dot(Arr_b,Arr_b_T)
-Arr_Arr_T_g = np.dot(Arr_g,Arr_g_T)
-Arr_Arr_T_r = np.dot(Arr_r,Arr_r_T)
 
-# compression rate
-compress = 100
-new_m = int(compress/100*m)
-new_n = int(compress/100*n)
+k = 99
 
-# hitung eigen value dan eigen vector matriks blue, untuk sementara (testing)
-w,v = np.linalg.eig(Arr_Arr_T_b)
-w = np.flip(w, axis=0)
-v = np.flip(v, axis=1)
-print('E-value:', w)
-print('E-vector', v)
+Ub, Sb, V_Tb = svd(Arr_b, k)
+Ug, Sg, V_Tg = svd(Arr_g, k)
+Ur, Sr, V_Tr = svd(Arr_r, k)
 
-# Matriks U (Singular kiri)
-U = np.zeros((m,new_m))
-print(U.shape)
-for i in range(m):
-    for j in range(new_m):
-        U[i][j] = v[i][j]
-print(U)
+new_matrix1 = np.dot(Ub,Sb)
+new_matrix1 = np.dot(new_matrix1,V_Tb)
+new_matrix2 = np.dot(Ug,Sg)
+new_matrix2 = np.dot(new_matrix2,V_Tg)
+new_matrix3 = np.dot(Ur,Sr)
+new_matrix3 = np.dot(new_matrix3,V_Tr)
 
-# Matriks Sigma
-S = np.zeros((new_m,new_n))
-k = min(new_m,new_n)
-print(S.shape)
-for i in range(k):
-    for j in range(k):
-        S[i][j] = w[i]
+new_matrix = cv2.merge([new_matrix1, new_matrix2, new_matrix3])
+print(new_matrix)
 
-# hitung eigen value dan eigen vector matriks blue, untuk sementara (testing)
-w,v = np.linalg.eig(Arr_T_Arr_b)
-w = np.flip(w, axis=0)
-v = np.flip(v, axis=1)
-print('E-value:', w)
-print('E-vector', v)
-v_T = np.transpose(v)
-
-# Matriks V (Singular kanan)
-V = np.zeros((new_n,n))
-for i in range(new_n):
-    for j in range(n):
-        V[i][j] = v_T[i][j]
-
-new_matrix = np.dot(U,S)
-new_matrix = np.dot(new_matrix,V)
-
-img = Image.fromarray(new_matrix, 'L')
+img = Image.fromarray(new_matrix.astype(np.uint8), 'RGB')
 img.save('Test Image/Test.jpg')
 img.show()
-
-"""
-for i in range(n):
-    norm = np.linalg.norm(v[i])
-    V[i] = v[i]/norm
-print(V)
-"""
-
-"""
-A = np.array([[3,4,3],[1,2,3],[4,2,1],[3,2,4]])
-row = A.shape[0]
-A2 = [[0 for j in range (4)] for i in range (3)]
-# convert array ke image lagi, keterangan: 'L' untuk grayscale, 'RGB' untuk berwarna
-img = Image.fromarray(r, 'L')
-img.save('Test.jpg')
-img.show()
-
-A = np.array([[10,0,2],[0,10,4],[2,4,2]])
-w,v = np.linalg.eig(A)
-print('E-value:', w)
-print('E-vector', v)
-w = np.flip(w, axis=0)
-v = np.flip(v, axis=1)
-print('E-value:', w)
-print('E-vector', v)
-"""
 
 # Timer Stop
 stop = timeit.default_timer()
