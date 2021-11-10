4191312088a5454bc209269ae29d7d4c00498cf0
HanselTanoto <hansel.tanoto05@gmail.com>
2021-11-10 11:50:26 +0700

    add compress function

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ import svd              # fungsi/prosedur menghitung svd (svd.py)
 start = timeit.default_timer()
 
 # Open image
-image = Image.open(r"Test Image\Coffee.jpg")
+image = Image.open(r"test\Coffee.jpg")
 M = np.asarray(image).astype(float)       # convert image to matrix
 print(M)
 M_b, M_g, M_r = cv2.split(M)              # split matriks ke matriks RGB
@@ -17,21 +17,27 @@ M_b, M_g, M_r = cv2.split(M)              # split matriks ke matriks RGB
 compress = int(input("Skala kompresi (%): "))
 
 # matriks U, S, V_T masing masing warna (RGB)
-Ub, Sb, V_Tb = svd(M_b, compress)
-Ug, Sg, V_Tg = svd(M_g, compress)
-Ur, Sr, V_Tr = svd(M_r, compress)
+Ub, Sb, V_Tb = svd.matrixSVD(M_b)
+Ug, Sg, V_Tg = svd.matrixSVD(M_g)
+Ur, Sr, V_Tr = svd.matrixSVD(M_r)
+
+# mengambil hanya mxk matriks U, kxk S, dan kxn matriks V_T
+nUb, nSb, nV_Tb = svd.compress(M_b, Ub, Sb, V_Tb, compress)
+nUg, nSg, nV_Tg = svd.compress(M_g, Ug, Sg, V_Tg, compress)
+nUr, nSr, nV_Tr = svd.compress(M_r, Ur, Sr, V_Tr, compress)
 
 # membentuk matriks kembali dari U, S, V_T
-new_Mb = svd.multiplySVD(Ub, Sb, V_Tb)
-new_Mg = svd.multiplySVD(Ug, Sg, V_Tg)
-new_Mr = svd.multiplySVD(Ur, Sr, V_Tr)
+new_Mb = svd.multiplySVD(nUb, nSb, nV_Tb)
+new_Mg = svd.multiplySVD(nUg, nSg, nV_Tg)
+new_Mr = svd.multiplySVD(nUr, nSr, nV_Tr)
 
 # menggabungkan matriks RGB
 new_M = cv2.merge([new_Mb, new_Mg, new_Mr])
+new_M = np.clip(new_M, 0, 255)
 print(new_M)
 # convert matriks RGB ke image
 img = Image.fromarray(new_M.astype(np.uint8), 'RGB')
-img.save('Test Image/Test.jpg')
+img.save('test\Test.jpg')
 img.show()
 
 # Timer Stop
--- src/svd.py
+++ src/svd.py
@@ -1,55 +1,47 @@
-import math
 import numpy as np
+import vektor
 
-
-def svd(M, compress):
-# Menghitung nilai Matriks U, S, dan V_T dari matriks M dengan skala kompresi (compress)
+def matrixSVD(M):
     M_T = np.transpose(M)       # transpose(M)
     M_M_T = np.dot(M, M_T)      # M*transpose(M)
     M_T_M = np.dot(M_T, M)      # transpose(M)*M
-    m, n = M.shape              # dimensi M
-    dim = min(m,n)
-    k = int(compress/100*dim)   # faktor kompresi
     
     # Matriks U
     e_val1, e_vec1 = np.linalg.eigh(M_M_T)
     e_val1 = np.flip(e_val1, axis=0)
     e_vec1 = np.flip(e_vec1, axis=0)
-    U = np.zeros((m,k))
-    for i in range(m):
-        for j in range(k):
-            U[i][j] = e_vec1[i][j]
-    
+    U = e_vec1
+
     # Matriks Sigma
     e_val2, e_vec2 = np.linalg.eigh(M_T_M)
     e_val2 = np.flip(e_val2, axis=0)
     e_vec2 = np.flip(e_vec2, axis=0)
-    S = np.zeros((k,k))
-    for i in range(k):
-        S[i][i] = math.sqrt(e_val2[i])
-    
+    S = np.sqrt(e_val2)
+
     # Matriks V_T
-    e_vec1_i = np.linalg.inv(e_vec1)
-    # Cari matriks U invers
-    U_i = np.zeros((k,m))
-    for i in range(k):
-        for j in range(m):
-            U_i[i][j] = e_vec1_i[i][j]
-    # Cari matriks S invers
-    S_i = np.linalg.inv(S)
-    # Hitung V_T = S_i*V_i*M
-    V_T = np.dot(U_i,M)
-    V_T = np.dot(S_i,V_T)
+    U_i = np.linalg.inv(U)                      # Cari matriks U invers
+    S_i = 1/S                                   # Cari matriks S invers
+    V_T = np.dot(np.diag(S_i),np.dot(U_i,M))    # Hitung V_T = S_i*V_i*M
 
     # Dimensi U, S, V_T
     print(U.shape)
     print(S.shape)
     print(V_T.shape)
+
     return U, S, V_T
 
 
-def multiplySVD(U, S, V_T):
+def compress(M, U, S, V_T, rate):
+# mengambil hanya mxk matriks U, kxk S, dan kxn matriks V_T
+    m,n = M.shape
+    k = int(rate/100*max(m,n))
+    nU = U[:, 0:k]
+    nS = S[0:k]
+    nV_T = V_T[0:k, :]
+    return nU, nS, nV_T
+
+
+def multiplySVD(nU, nS, nV_T):
 # Menghitung hasil perkalian matriks U, S, dan V_T 
-    result = np.dot(U,S)
-    result = np.dot(result,V_T)
+    result = np.dot(np.dot(nU,np.diag(nS)),nV_T)
     return result
